4dc3de88bf774482abb10b5844fdcb02578e7e5f
Mathieu Nagle <62767413+Mathieu-Nagle@users.noreply.github.com>
2022-08-16 23:26:47 -0400

    Maxed Minmax into it's own file

--- webdevapp/src/App.js
+++ webdevapp/src/App.js
@@ -1,30 +1,14 @@
-import logo from './aq-torq.png'
-import './App.css';
-import * as React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { getminmaxdata,rAvgTimeSeries } from './getdata.service.js';
+import logo from "./aq-torq.png";
+import "./App.css";
+import * as React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Minmax } from "./Minmax.js";
+import { rAvgTimeSeries } from "./getdata.service.js";
 
 function App() {
-  let day = new Date();
-  const today = day.getFullYear()+'.'+(day.getMonth()+1)+'.'+day.getDate();
-  const yesterday = day.getFullYear()+'.'+(day.getMonth()+1)+'.'+(day.getDate()-1);
-  const twodaysago = day.getFullYear()+'.'+(day.getMonth()+1)+'.'+(day.getDate()-2);
-  
-  const [date, setdate] = React.useState(today);
-  const [data, getData] = React.useState([]);
-
-  React.useEffect(() => {
-    getminmaxdata(date)
-    .then((response) => {
-      console.log(response);
-      getData(response);
-    })
-  }, [date])
-
-  rAvgTimeSeries()
-  .then((response) => {
+  rAvgTimeSeries().then((response) => {
     console.log(response);
-  })
+  });
 
   return (
     <div className="App">
@@ -32,53 +16,23 @@ function App() {
         <h1>SlaQers</h1>
       </header>
       <body className="App-body">
-      <div className="row container text-center">
-        <div className="col-md-2">
-          <ul className="nav flex-column">
-            {/* <li>
+        <div className="row container text-center">
+          <div className="col-md-2">
+            <ul className="nav flex-column">
+              {/* <li>
               <image src={logo} className="App-logo" alt="logo" />
             </li> */}
-            <li>Moving Average Graph</li>
-            <li>Volotility Graph</li>
-            <li>Current Price</li>
-            <li>Cast U-cosle Table</li>
-            <li>Min/Max</li>
-            <li>Summary Volume(high)</li>
-          </ul>
-        </div>
-      <div className="col">
-        <select className="form-select" value={date} onChange={(event) => {setdate(event.target.value)}}>
-          <option value={today}>{today}</option>
-          <option value={yesterday}>{yesterday}</option>
-          <option value={twodaysago}>{twodaysago}</option>
-        </select>
-        <div>
-          <h1>MinMax</h1>
-            <div className='item-container'>
-              {data.map((item) => (
-              <div className='card'>
-                <h3>{item.sym}</h3>
-                <p>Max:${item.Max} Min:${item.Min}</p>
-                {/* <p>{item.Min}</p> */}
-              </div>
-              ))}
-            </div>
-        </div>
-        {/* <tbody className="table table-bordered">
-          <tr>
-            <th>Sym</th>
-              <th>Max Price</th>
-              <th>Min Price</th>
-          </tr>
-          {data.map((item, i) => (
-          <tr key={i}>
-            <td>{item.sym}</td>
-            <td>${item.Max}</td>
-            <td>${item.Min}</td>
-          </tr>
-          ))}
-        </tbody> */}
-        </div>
+              <li>Moving Average Graph</li>
+              <li>Volotility Graph</li>
+              <li>Current Price</li>
+              <li>Cast U-cosle Table</li>
+              <li>Min/Max</li>
+              <li>Summary Volume(high)</li>
+            </ul>
+          </div>
+          <div className="col">
+            <Minmax />
+          </div>
         </div>
       </body>
     </div>
---
+++ webdevapp/src/Minmax.js
@@ -0,0 +1,54 @@
+import "./App.css";
+import * as React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { getminmaxdata } from "./getdata.service.js";
+
+export const Minmax = () => {
+  let day = new Date();
+  const today =
+    day.getFullYear() + "." + (day.getMonth() + 1) + "." + day.getDate();
+  const yesterday =
+    day.getFullYear() + "." + (day.getMonth() + 1) + "." + (day.getDate() - 1);
+  const twodaysago =
+    day.getFullYear() + "." + (day.getMonth() + 1) + "." + (day.getDate() - 2);
+
+  const [date, setdate] = React.useState(today);
+  const [data, getData] = React.useState([]);
+
+  React.useEffect(() => {
+    getminmaxdata(date).then((response) => {
+      console.log(response);
+      getData(response);
+    });
+  }, [date]);
+
+  return (
+    <main>
+      <select
+        className="form-select"
+        value={date}
+        onChange={(event) => {
+          setdate(event.target.value);
+        }}
+      >
+        <option value={today}>{today}</option>
+        <option value={yesterday}>{yesterday}</option>
+        <option value={twodaysago}>{twodaysago}</option>
+      </select>
+      <div>
+        <h1>MinMax</h1>
+        <div className="item-container">
+          {data.map((item) => (
+            <div className="card">
+              <h3>{item.sym}</h3>
+              <p>
+                Max:${item.Max} Min:${item.Min}
+              </p>
+              {/* <p>{item.Min}</p> */}
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+};
